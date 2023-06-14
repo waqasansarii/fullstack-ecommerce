fa67b53da00264fc1bebede755908c5ac4af3d40
Muhammad Waqas <ansariwaqas310@gmail.com>
2023-06-14 15:15:18 +0500

    added cart updated api and stripe integration

---
+++ src/app/api/create-stripe-session/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2022-11-15",
+});
+
+
+export  async function POST(req: any, res: NextResponse){
+    const {item}= await req.json();
+
+    const transformedItem = {
+         price_data: {
+          currency: 'usd',
+          product_data:{
+            name: item.product_name,
+            category: item.product_category,
+            images:[item.product_image],
+            metadata:{},
+
+          },
+          unit_amount: item.price * 100,
+
+        },
+        quantity: item.quantity,
+        
+      };
+      const redirectURL =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'your deployed url';
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['link'],
+        line_items: [transformedItem],
+        mode: 'payment',
+        success_url: redirectURL + '/payment/success',
+        cancel_url: redirectURL + '/payment/fail',
+        metadata: {
+          images: item.image,
+          name:"some additional info",
+          task:"Waqas Product"
+        },
+      
+      });
+
+    //    console.log("response-------------------",await session);
+    return NextResponse.json(session?.id) ;
+  };
--- src/app/cart/priceCard.tsx
+++ src/app/cart/priceCard.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { CartItem } from '@/lib/type'
-import React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import React, { useState } from 'react'
 
 const PriceCard = ({ data }: { data: CartItem[] }) => {
+  const [loading , setLoading] = useState(false)
   let countQty = data.reduce((acc: number, val: CartItem) => {
     // console.log(acc,val)
     let qty = acc + val.quantity
@@ -15,6 +17,40 @@ const PriceCard = ({ data }: { data: CartItem[] }) => {
     return totalPrice
   }, 0)
 
+
+  const publishableKey = process.env
+  .NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string;
+const stripePromise = loadStripe(publishableKey);
+
+const createCheckOutSession = async () => {
+  setLoading(true);
+  const stripe = await stripePromise;
+
+  const checkoutSession = await fetch(
+    "http://localhost:3000/api/create-stripe-session",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        item: data,
+      }),
+    }
+  );
+
+    console.log("Result------------- in prod page==========",  checkoutSession);
+
+  const sessionID= await checkoutSession.json();
+  const result = await stripe?.redirectToCheckout({
+    sessionId: sessionID,
+  });
+  if (result?.error) {
+    alert(result.error.message);
+  }
+  setLoading(false);
+};
+
   return (
     <div className="lg:w-[30%] w-full p-5 rounded-md bg-blue-100/50">
       <h2 className="text-2xl font-bold">Order Summary</h2>
@@ -26,7 +62,7 @@ const PriceCard = ({ data }: { data: CartItem[] }) => {
         <h5 className="text-md font-bold">Total</h5>
         <p>${countPrice}</p>
       </div>
-      <button className="bg-black py-2 px-5 rounded-lg w-full mt-3 text-white">
+      <button onClick={createCheckOutSession} className="bg-black py-2 px-5 rounded-lg w-full mt-3 text-white">
         Process to checkout
       </button>
     </div>
